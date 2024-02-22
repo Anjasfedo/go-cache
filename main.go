bd3d64f8d36e6e892c215442f388ba35281e59d6
Anjasfedo <fedoaf98@gmail.com>
2024-02-22 23:29:45 +0700

    Define the Structs

--- main.go
+++ main.go
@@ -4,6 +4,30 @@ import (
 	"fmt"
 )
 
+type Node struct {
+	Left
+
+	Right
+}
+
+type Queue struct {
+	Node
+}
+
+type Cache struct {
+	Queue Queue
+	Hash Hash
+}
+
+type Hash map[string]*Node
+
 func main() {
 	fmt.Println("Start Cache")
+
+	cache := NewCache()
+
+	for _, word := range []string{"banana", "papaya", "potato", "tomato", "tree", "cat"} {
+		cache.Check(word)
+		cache.Display()
+	}
 }
